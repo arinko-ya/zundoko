135bbe4e1351e83d96ff770a94aa17328df51301
arinko_ya <shiroper.yasaku@gmail.com>
2018-06-02 10:07:49 +0900

    zundoko!

---
+++ zundoko.py
@@ -0,0 +1,16 @@
+import random
+
+
+def zundoko() -> None:
+    zundoko_list = []
+    for i in range(50):
+        selected_string = random.choice(['ズン', 'ドコ'])
+        print(selected_string)
+        zundoko_list.append(zundoko)
+        if 'ズンズンズンドコ' in ''.join(zundoko_list):
+            print('キヨシ！！')
+            zundoko_list.clear()
+
+
+if __name__ == '__main__':
+    zundoko()
