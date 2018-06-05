9d3faae45924fa6c1e1c099bec20e86ef74d2ac8
arinko_ya <shiroper.yasaku@gmail.com>
2018-06-05 21:53:08 +0900

    fix

--- zundoko.py
+++ zundoko.py
@@ -6,7 +6,7 @@ def zundoko() -> None:
     for i in range(50):
         selected_string = random.choice(['ズン', 'ドコ'])
         print(selected_string)
-        zundoko_list.append(zundoko)
+        zundoko_list.append(selected_string)
         if 'ズンズンズンドコ' in ''.join(zundoko_list):
             print('キヨシ！！')
             zundoko_list.clear()
